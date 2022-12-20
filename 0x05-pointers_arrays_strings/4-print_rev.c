8d305c15766dda22ddb78167c468a34933bb9aaf
ShinaJr <lasisioluwashina@gmail.com>
2022-12-20 14:56:12 -0800

    0x05-pointers_arrays_strings

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -3,15 +3,15 @@
  * print_rev - print a string in reverse
  * @s: char array string type
  */
-void print_rev(char *s)
 {
-	int i;
+	int len;
 
-	for (i = 0; s[i] != '\0'; i++)
-
-	for (i--; i >= 0; i--)
-		_putchar(s[i]);
-	
+	for (len = 0; s[len] != '\0'; len++)
+	{
+	}
+	for (len = len - 1; len >= 0; len--)
+	{
+		_putchar(s[len]);
+	}
 	_putchar('\n');
-
 }
