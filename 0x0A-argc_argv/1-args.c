109002b55974ab16c931042bb70d7d4ea4715181
ShinaJr <lasisioluwashina@gmail.com>
2023-01-06 03:46:13 -0800

    0x0A-argc_argv

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,14 +1,15 @@
 #include "main.h"
 #include <stdio.h>
 /**
- *  * main - print the num of args you passed to it
- *   * @argc: argument count
- *    * @argv: argument vector, array of strings
- *     * Return: 0
- *      */
+ * main - prints the number of arguenments passed to it
+ * @argc: arguemnet count
+ * @argv: array of pointer to arguement strings
+ * Return: 0
+ **/
 int main(int argc, char *argv[])
 {
-	(void) argv;
+/* to suppress unused variable warning */
+	argv = argv;
 	printf("%d\n", argc - 1);
 	return (0);
 }
